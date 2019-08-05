4221e6cc59baf22ac348a2eac887066274c9ace1
Kamule42 <benjamin.herbomez@gmail.com>
2019-08-05 14:06:09 +0200

    2 files modified, 1 added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ValidateEqualDirective } from './validate-equal.directive';
+import { ValidatePasswordDirective } from './validate-password.directive';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ValidateEqualDirective],
-  exports : [ValidateEqualDirective]
+  declarations: [ValidateEqualDirective, ValidatePasswordDirective],
+  exports : [ValidateEqualDirective, ValidatePasswordDirective]
 })
 export class SharedModule { }
---
+++ src/app/shared/validate-password.directive.ts
@@ -0,0 +1,44 @@
+import { Directive, Input } from '@angular/core';
+import { Validator, AbstractControl, NG_VALIDATORS } from '@angular/forms';
+
+@Directive({
+  selector: '[validatePassword][formControlName],[validatePassword][formControl],[validatePassword][ngModel]',
+  providers: [{provide: NG_VALIDATORS, useExisting: ValidatePasswordDirective, multi: true}]
+})
+export class ValidatePasswordDirective {
+
+  validate(c: AbstractControl): { [key: string]: any } {
+        // self value (e.g. retype password)
+        let v = c.value;
+        // value not equal
+        if (v) {
+          let containsUpperCase = false;
+          let containsLowerCase = false;
+          let containerNumber = false;
+          let containsSpecial = false;
+          v.split('').forEach(c => {
+            if(/^[a-zA-Z0-9- ,_]*$/.test(c) == false){
+                containsSpecial = true;
+            }
+            else if (!isNaN(c * 1)){
+              containerNumber = true;
+            }
+            else{
+              if (c == c.toUpperCase()) {
+                containsUpperCase = true;
+              }
+              if (c == c.toLowerCase()){
+                containsLowerCase = true;
+              }
+            }
+          });
+          let ok = +containsUpperCase+(+containsLowerCase)+(+containerNumber)+(+containsSpecial) >= 3;
+          if(!ok)  return {
+            validatePassword: false
+          }
+        }
+        return null;
+    }
+
+
+}
