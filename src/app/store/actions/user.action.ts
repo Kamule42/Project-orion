7e5f75afc63720450c4a241873967e26944fad71
Kamule42 <benjamin.herbomez@gmail.com>
2019-08-08 14:18:44 +0200

    1 file modified

--- src/app/store/actions/user.action.ts
+++ src/app/store/actions/user.action.ts
@@ -7,31 +7,36 @@ export enum UserActions {
   SIGN_WITH_PROVIDER  = '[User Component] Sign with porvider',
   SIGNED_IN           = '[User Component] Sign in',
   SIGNE_OUT           = '[User Component] Sign out',
-  DEFINE_USERNAME     = '[User Component] define username'
+  DEFINE_USERNAME     = '[User Component] define username',
+  SIGNED_ERROR        = '[User Component] sign error',
 }
 
 export const signupWithEmail = createAction(
   UserActions.SIGNUP_WITH_EMAIL,
-  props<{email: string; password: string}>()
+  props<{payload:{email: string; password: string}}>()
 );
 
 export const signinWithEmail = createAction(
   UserActions.SIGNIN_WITH_EMAIL,
-  props<{email: string; password: string}>()
+  props<{payload:{email: string; password: string}}>()
 );
 
 export const signWithProvider = createAction(
   UserActions.SIGN_WITH_PROVIDER,
-  props<{provider:string}>()
+  props<{payload:{provider:string}}>()
 );
 
 export const signedIn = createAction(
   UserActions.SIGNED_IN,
-  props<{user:User}>()
+  props<{payload:{user:User}}>()
 );
 
 export const signout = createAction(UserActions.SIGNE_OUT);
 
 export const defineUsername = createAction(
   UserActions.DEFINE_USERNAME,
-  props<{username:string}>());
+  props<{payload:{username:string}}>());
+
+export const signError = createAction(
+  UserActions.SIGNED_ERROR,
+  props<{payload:{error:any}}>());
--- src/app/store/effects/user.effects.ts
+++ src/app/store/effects/user.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, createEffect, ofType } from '@ngrx/effects';
-import { EMPTY, from  } from 'rxjs';
+import { EMPTY, from, of } from 'rxjs';
 import { catchError, switchMap, map } from 'rxjs/operators';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
@@ -10,24 +10,37 @@ import { UserActions } from '../actions/user.action';
 @Injectable()
 export class UserEffects {
  
-  signInWithEmail$ = createEffect(() => 
-    this.actions$.pipe(
-      ofType(UserActions.SIGNIN_WITH_EMAIL),
-      switchMap(action => from (this.afAuth.auth.signInWithEmailAndPassword(
-         action.email, action.password))
-         .map(result => {
-           type: UserActions.SIGNED_IN,
-           payload:result
-         ))
-         .catchError((error)=> of({
-           type: UserActions.SIGNED_ERROR
-         }))
-      ),
-    )
-  );
+  signInWithEmail$;
  
   constructor(
     private actions$: Actions,
     public afAuth: AngularFireAuth,
-  ) {}
+  ) {
+    this.init()
+  }
+
+  private init(){
+    this.initSignWithEmail();
+  }
+
+  private initSignWithEmail(){
+    this.signInWithEmail$ = createEffect(() => 
+    this.actions$.pipe(
+      ofType(UserActions.SIGNIN_WITH_EMAIL),
+      switchMap((action:any) => from (this.afAuth.auth.signInWithEmailAndPassword(
+         action.payload.email, action.payload.password))
+         .pipe(
+            map(result => ({
+                type: UserActions.SIGNED_IN,
+                payload:result
+            }),
+            catchError((error)=> of({
+              type: UserActions.SIGNED_ERROR,
+              payload: null
+            }))
+           )
+        ),
+      )
+    ));
+  }
 }
